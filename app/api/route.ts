8dd333a0ae37c463e8aac97561904c37f03d2378
arkantos <abbas_mhosseini@yahoo.com>
2023-10-09 19:32:06 +0330

    add req

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,13 +1,5 @@
 import axios from 'axios'
 import { NextResponse } from 'next/server'
-
-const axiosInstance = axios.create({
-  headers: {
-    'Cache-Control': 'no-cache',
-    Pragma: 'no-cache',
-    Expires: '0',
-  },
-})
 const apiUrl =
   'https://coin360.com/site-api/coins?currency=USD&period=1h&ranking=top100'
 
@@ -28,7 +20,7 @@ interface Coin360 {
 }
 
 async function FetchData() {
-  const response = await axiosInstance.get(apiUrl)
+  const response = await axios.get(apiUrl)
   const coinData = response.data.data
 
   const sorted = coinData.sort((a: Coin360, b: Coin360) => a.ch - b.ch)
@@ -39,7 +31,7 @@ async function FetchData() {
   return { labels, changes }
 }
 
-export async function GET() {
+export async function GET(request: Request) {
   const res = await FetchData()
   return NextResponse.json(res)
 }
